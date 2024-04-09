e15cba1d6c72d8ba57a16b1ddda0eabbf0a581df
Jirapat27 <jirapat.jira@kmutt.ac.th>
2024-04-09 19:05:19 +0700

    Update index.js

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ import CommentForm from "./App/Component/CommentForm";
 import Comment from "./App/Component/Comment";
 import ChoosePlace from "./App/Screen/Calculate/ChoosePlace";
 import ChooseWin from "./App/Screen/Calculate/ChooseWin";
+import Report from "./App/Screen/Menu/Report";
 
 const Stack = createStackNavigator();
 
@@ -61,6 +62,8 @@ export default function AppIndex() {
           component={ChoosePlace} />
         <Stack.Screen name="ChooseWin"
           component={ChooseWin} />
+        <Stack.Screen name="Report"
+          component={Report}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
