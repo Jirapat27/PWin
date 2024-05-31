66fc46375978c56b4db4a0d13c4e8a0bdec3e8f3
Jirapat27 <jirapat.jira@kmutt.ac.th>
2024-05-31 22:36:11 +0700

    Update Login.jsx

--- src/App/Login.jsx
+++ src/App/Login.jsx
@@ -56,7 +56,7 @@ const Login = ({ history }) => {
     <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
         <h2 className="mt-6 text-center text-3xl font-extrabold text-orange-500">
-          LOGIN
+          P'Win's Admin Login
         </h2>
       </div>
       <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
