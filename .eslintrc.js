1541e89661fa91bc9e5cde59bb04067d16802e1a
Jirapat27 <jirapat.jira@kmutt.ac.th>
2024-06-10 12:25:30 +0700

    final_version

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,5 +3,9 @@ module.exports = {
   extends: '@react-native',
   rules: {
     'prettier/prettier': 0,
+    'react-native/no-inline-styles': 0,
+    'no-trailing-spaces': 0,
+    'quotes': 1,
+    'no-shadow': 0,
   },
 };
--- index.js
+++ index.js
@@ -6,5 +6,11 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import 'react-native-gesture-handler';
+import { LogBox } from 'react-native';
+
+// Ignore specific warning
+LogBox.ignoreLogs(['Image source "null" doesn\'t exist']);
+LogBox.ignoreLogs(['Request has not been opened']);
+LogBox.ignoreLogs(['VirtualizedLists should never be nested inside plain ScrollViews with the same orientation because it can break windowing and other functionality - use another VirtualizedList-backed container instead.']);
 
 AppRegistry.registerComponent(appName, () => App);
